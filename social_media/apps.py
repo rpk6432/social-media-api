b031ca98732f1bbb051f294e39229780c14beb6d
Roman Peka <pekaromanmail@gmail.com>
2025-09-06 15:29:08 +0300

    Add signal for automatic profile creation on user signup

--- social_media/apps.py
+++ social_media/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SocialMediaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "social_media"
+
+    def ready(self):
+        import social_media.signals
