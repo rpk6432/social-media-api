719effaca5d46f163617e98c778195d64e64590c
Roman Peka <pekaromanmail@gmail.com>
2025-09-08 20:10:36 +0300

    Add UserViewSet with registration, me endpoint, and JWT routes

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,20 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("social_media.urls", namespace="social_media")),
+    path(
+        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
--- social_media/urls.py
+++ social_media/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from social_media.views import UserViewSet
 
 app_name = "social_media"
 
-urlpatterns = []
+router = DefaultRouter()
+router.register("users", UserViewSet, basename="users")
+
+urlpatterns = [path("", include(router.urls))]
