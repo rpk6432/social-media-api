ccc6927195400dfd6742c85b80c1e1d0ed9e7413
Roman Peka <pekaromanmail@gmail.com>
2025-09-08 20:10:36 +0300

    Add UserViewSet with registration, me endpoint, and JWT routes

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from typing import Type
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import (
+    IsAuthenticated,
+    AllowAny,
+    BasePermission,
+)
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+
+from social_media.serializers import (
+    UserRegistrationSerializer,
+    ProfileSerializer,
+    UserSerializer,
+)
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.select_related("profile")
+
+    def get_serializer_class(self) -> Type[Serializer]:
+        if self.action == "create":
+            return UserRegistrationSerializer
+        if self.action == "me" and self.request.method in ["PUT", "PATCH"]:
+            return ProfileSerializer
+        return UserSerializer
+
+    def get_permissions(self) -> list[BasePermission]:
+        if self.action == "create":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    @action(
+        detail=False,
+        methods=["get", "put", "patch"],
+        permission_classes=[IsAuthenticated],
+        url_path="me",
+    )
+    def me(self, request: Request, *args, **kwargs) -> Response:
+        user = request.user
+        if request.method == "GET":
+            serializer = self.get_serializer(user)
+            return Response(serializer.data)
+
+        profile = user.profile
+        serializer = self.get_serializer(
+            profile, data=request.data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(UserSerializer(user).data)
