590ba28abc2abecf68deff5b45bf18bd002094ea
Roman Peka <pekaromanmail@gmail.com>
2025-09-15 16:40:58 +0300

    Add like/unlike & follow/unfollow actions to viewsets

--- social_media/views.py
+++ social_media/views.py
@@ -2,7 +2,7 @@ from typing import Type
 
 from django.contrib.auth import get_user_model
 from django.db.models import QuerySet, Count
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import (
@@ -14,7 +14,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 
-from social_media.models import Post, Comment
+from social_media.models import Post, Comment, Like, Follow
 from social_media.permissions import IsOwnerOrReadOnly
 from social_media.serializers import (
     UserRegistrationSerializer,
@@ -68,6 +68,54 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.save()
         return Response(UserSerializer(user).data)
 
+    @action(
+        methods=["POST"], detail=True, permission_classes=[IsAuthenticated]
+    )
+    def follow(self, request: Request, pk: int | None = None) -> Response:
+        """Follow a user."""
+        user_to_follow = self.get_object()
+        if user_to_follow == request.user:
+            return Response(
+                {"detail": "You cannot follow yourself."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        _, created = Follow.objects.get_or_create(
+            follower=request.user, following=user_to_follow
+        )
+
+        if not created:
+            return Response(
+                {"detail": "You already following this user."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(
+            {"detail": "Successfully followed the user."},
+            status=status.HTTP_200_OK,
+        )
+
+    @action(
+        methods=["POST"], detail=True, permission_classes=[IsAuthenticated]
+    )
+    def unfollow(self, request: Request, pk: int | None = None) -> Response:
+        """Unfollow a user."""
+        user_to_unfollow = self.get_object()
+
+        deleted_count, _ = Follow.objects.filter(
+            follower=request.user, following=user_to_unfollow
+        ).delete()
+
+        if deleted_count == 0:
+            return Response(
+                {"detail": "You don't following this user."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(
+            {"detail": "Successfully unfollowed the user."},
+        )
+
 
 class PostViewSet(viewsets.ModelViewSet):
     def get_queryset(self) -> QuerySet:
@@ -97,6 +145,43 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer: Serializer) -> None:
         serializer.save(user=self.request.user)
+
+    @action(
+        methods=["post"], detail=True, permission_classes=[IsAuthenticated]
+    )
+    def like(self, request: Request, pk: int | None = None) -> Response:
+        """Add a like to the post"""
+        post = self.get_object()
+        user = request.user
+        _, created = Like.objects.get_or_create(user=user, post=post)
+
+        if not created:
+            return Response(
+                {"detail": "You already liked this post."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return Response(
+            {"detail": "Post liked successfully."}, status=status.HTTP_200_OK
+        )
+
+    @action(
+        methods=["post"], detail=True, permission_classes=[IsAuthenticated]
+    )
+    def unlike(self, request: Request, pk: int | None = None) -> Response:
+        """Remove a like from the post"""
+        post = self.get_object()
+        user = request.user
+        deleted_count, _ = Like.objects.filter(user=user, post=post).delete()
+
+        if deleted_count == 0:
+            return Response(
+                {"detail": "You have not liked this post yet."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return Response(
+            {"detail": "Like removed successfully."},
+            status=status.HTTP_200_OK,
+        )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
